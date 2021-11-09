d890273929fd5e7ec96e64850648a7f8693f85bc
natserract <benjaminstwo@gmail.com>
2021-11-09 21:49:55 +0800

    refactor: change getCurrentUser

--- api/src/lib/auth.ts
+++ api/src/lib/auth.ts
@@ -1,25 +1,58 @@
-/**
- * Once you are ready to add authentication to your application
- * you'll build out requireAuth() with real functionality. For
- * now we just return `true` so that the calls in services
- * have something to check against, simulating a logged
- * in user that is allowed to access that service.
- *
- * See https://redwoodjs.com/docs/authentication for more info.
- */
-export const isAuthenticated = () => {
-  return true
-}
+import { AuthenticationError } from '@redwoodjs/graphql-server'
+import admin from 'firebase-admin'
+import { logger } from './logger'
 
-export const hasRole = ({ roles }) => {
-  return roles !== undefined
-}
-
-// This is used by the redwood directive
-// in ./api/src/directives/requireAuth
-
-// Roles are passed in by the requireAuth directive if you have auth setup
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const requireAuth = ({ roles }) => {
-  return isAuthenticated()
+const adminApp = admin.initializeApp({
+  projectId: process.env.FIREBASE_PROJECT_ID,
+})
+
+/**
+ * getCurrentUser returns the user information together with
+ * an optional collection of roles used by requireAuth() to check
+ * if the user is authenticated or has role-based access
+ *
+ * @param decoded - The decoded access token containing user info and JWT claims like `sub`. Note could be null.
+ * @param { token, SupportedAuthTypes type } - The access token itself as well as the auth provider type
+ * @param { APIGatewayEvent event, Context context } - An object which contains information from the invoker
+ * such as headers and cookies, and the context information about the invocation such as IP Address
+ *
+ * @see https://github.com/redwoodjs/redwood/tree/main/packages/auth for examples
+ */
+export const getCurrentUser = async (decoded, { type, token }) => {
+  logger.debug(
+    {
+      payload: { decoded, type, token },
+    },
+    'Current User'
+  )
+
+  return { decoded, type, token }
+}
+/**
+ * The user is authenticated if there is a currentUser in the context
+ *
+ * @returns {boolean} - If the currentUser is authenticated
+ */
+export const isAuthenticated = (): boolean => {
+  return !!context.currentUser
+}
+
+/**
+ * Call requireAuth in your services, or use the @requireAuth directive to check that a user is logged in,
+ * and raise an error if they're not.
+ *
+ * @returns - If the currentUser is authenticated
+ *
+ * @throws {AuthenticationError} - If the currentUser is not authenticated
+ *
+ * @see https://github.com/redwoodjs/redwood/tree/main/packages/auth for examples
+ */
+export const requireAuth = () => {
+  if (!isAuthenticated()) {
+    throw new AuthenticationError("You don't have permission to do that.")
+  }
+
+  // Custom RBAC implementation required for firebase
+  // https://firebase.google.com/docs/auth/admin/custom-claims
 }
