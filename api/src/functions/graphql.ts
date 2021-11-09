880ee0fc178b38b53dc94023cce7a183cb2c748f
natserract <benjaminstwo@gmail.com>
2021-11-09 21:48:22 +0800

    feat: add cors settings

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -4,14 +4,21 @@ import directives from 'src/directives/**/*.{js,ts}'
 import sdls from 'src/graphql/**/*.sdl.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
 
+import { getCurrentUser } from 'src/lib/auth'
+
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
 
 export const handler = createGraphQLHandler({
+  getCurrentUser,
   loggerConfig: { logger, options: {} },
   directives,
   sdls,
   services,
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     db.$disconnect()
